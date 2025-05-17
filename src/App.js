0581c4e379e00c7b1eb92c358319d0926d5ffe86
DarshanLevi <s2darshan2@gmail.com>
2025-05-17 19:00:13 +0530

    update: 10

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Shop from "./Pages/Shop.jsx";
-import ShopCategory from "./Pages/ShopCategory.jsx";
-import Product from "./Pages/product.jsx";
-import Cart from "./Pages/Cart.jsx";
-import LoginSignup from "./Pages/LoginSignup.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
-import men_banner from "./Components/Assets/banner_mens.png";
-import women_banner from "./Components/Assets/banner_women.png";
-import kid_banner from "./Components/Assets/banner_kids.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+// Lazy load route components
+const Shop = lazy(() => import("./Pages/Shop.jsx"));
+const ShopCategory = lazy(() => import("./Pages/ShopCategory.jsx"));
+const Product = lazy(() => import("./Pages/product.jsx"));
+const Cart = lazy(() => import("./Pages/Cart.jsx"));
+const LoginSignup = lazy(() => import("./Pages/LoginSignup.jsx"));
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route
-            path="/mens"
-            element={<ShopCategory banner={men_banner} category="men" />}
-          />
-          <Route
-            path="/women"
-            element={<ShopCategory banner={women_banner} category="women" />}
-          />
-          <Route
-            path="/kids"
-            element={<ShopCategory banner={kid_banner} category="kid" />}
-          />
-          <Route path="/product" element={<Product />}>
-            <Route path=":productId" element={<Product />} />
-          </Route>
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSignup />} />
-        </Routes>
+        <Suspense fallback={<div className="loading">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route
+              path="/mens"
+              element={
+                <ShopCategory
+                  banner={require("./Components/Assets/banner_mens.png")}
+                  category="men"
+                />
+              }
+            />
+            <Route
+              path="/women"
+              element={
+                <ShopCategory
+                  banner={require("./Components/Assets/banner_women.png")}
+                  category="women"
+                />
+              }
+            />
+            <Route
+              path="/kids"
+              element={
+                <ShopCategory
+                  banner={require("./Components/Assets/banner_kids.png")}
+                  category="kid"
+                />
+              }
+            />
+            <Route path="/product" element={<Product />}>
+              <Route path=":productId" element={<Product />} />
+            </Route>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<LoginSignup />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </BrowserRouter>
     </div>
